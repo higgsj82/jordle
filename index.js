b7641871276dce76ea20a7bbfe539e4f8bf12376
higgsj82 <higgsj82@gmail.com>
2022-03-02 20:08:29 -0500

    connected backend to Random Word API

---
+++ index.js
@@ -0,0 +1,28 @@
+const PORT = 3000;
+const axios = require("axios").default;
+const express = require("express");
+require('dotenv').config()
+
+const app = express()
+
+
+app.get('/word', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
+        params: { count: '5', wordLength: '5' },
+        headers: {
+            'x-rapidapi-host': 'random-words5.p.rapidapi.com',
+            'x-rapidapi-key': process.env.RAPID_API_KEY
+        }
+    };
+    
+    axios.request(options).then((response) => {
+        console.log(response.data);
+        res.json(response.data[0])
+    }).catch((error) => {
+        console.error(error);
+    });
+})
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT))
